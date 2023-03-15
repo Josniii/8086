6392ddb76376a97d927d44071ed96ddd7b8d5ded
Jonas Søndergaard <jonas@wexo.dk>
2023-03-15 18:42:26 +0100

    Use Casey's git directly for sourcing testfiles, as instructed in Q&A

--- test.py
+++ test.py
@@ -2,10 +2,10 @@
 
 import os
 
-for file in os.listdir("tests"):
+for file in os.listdir("tests/computer_enhance/perfaware/part1"):
     if not file.endswith(".asm"):
         out_file = os.path.join("out", file)
         with open(out_file, "w") as f:
-            os.system(f"8086 tests/{file} > {out_file}.asm")
+            os.system(f"8086 tests/computer_enhance/perfaware/part1/{file} > {out_file}.asm")
         os.system(f"nasm {out_file}.asm -o {out_file}")
-        os.system(f"diff {out_file} tests/{file}")
+        os.system(f"diff {out_file} tests/computer_enhance/perfaware/part1/{file}")
